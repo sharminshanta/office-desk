7e9a63695a5f3c1a520806acc87e969d21c72c8f
sharminex81 <mimetyshanta@gmail.com>
2017-07-30 02:24:31 +0600

    Put a model in autoload named roles

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('users');
+$autoload['model'] = array('users', 'roles');
