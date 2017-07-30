0597b9688bfe36a314cfbb50e8dfcb290173ba65
sharminex81 <mimetyshanta@gmail.com>
2017-07-30 14:32:12 +0600

    Added home page for application default page

---
+++ application/views/home.php
@@ -0,0 +1,51 @@
+<div class="container-fluid">
+    <div class="content-area">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Welcome
+                            , Firstname Lastname</h3>
+                    </div>
+                    <div class="panel-body text-center" id="leftPanel">
+                        <a class="btn btn-primary" href="/profile/general/update"
+                           title="Update your profile"><i class="fa fa-info"></i>
+                            Update Profile
+                        </a>
+                        <a class="btn btn-danger" href="/profile/security"
+                           title="Change account password">
+                            <i class="fa fa-user-secret"></i>
+                            Change Password
+                        </a>
+                        <a class="btn btn-info" href="/profile/security"
+                           title="Check security settings">
+                            <i class="fa fa-shield"></i>
+                            Review Security Settings
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Important Information</h3>
+                    </div>
+                    <div class="panel-body" id="rightPanel">
+                        <div class="notice notice-danger">
+                            <a target="_blank" href="https://besofty.com/">
+                                Learn more
+                            </a>
+                            To assign role permission
+                        </div>
+                        <div class="notice notice-info">
+                            <a target="_blank" href="https://besofty.com/">
+                                Learn more
+                            </a>
+                            To assign role permission
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
