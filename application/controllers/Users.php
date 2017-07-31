c2c0baa8a5c4b56caa419e15aaf1a81a3b228a61
sharminex81 <mimetyshanta@gmail.com>
2017-07-31 16:25:12 +0600

    Added a library named "vlucas/valitron"

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -9,6 +9,22 @@ class Users extends CI_Controller
     function __construct()
     {
         parent::__construct();
+
+        $user = $this->session->userdata('details');
+
+        if($user == null) {
+            $message['error'] = 'Sorry! Access Denied. You don’t have permission to do.';
+            $this->session->set_userdata($message);
+            redirect('login','refresh');
+        }
+
+        $role = Roles::getName($user['user']->role_id);
+
+        if($role->slug == 'general-user') {
+            $message['error'] = 'Sorry! Access Denied. You don’t have permission to do.';
+            $this->session->set_userdata($message);
+            redirect('login','refresh');
+        }
     }
 
     public function index()
@@ -22,25 +38,44 @@ class Users extends CI_Controller
     }
 
     /**
-     * This is user's add
+     * This is user's add method
      */
     public function create()
     {
         //@TODO try to manage it with the best way
 
-        /**
-         * Form validation with the codeigniter default formvalidation rules
-         */
-        $this->form_validation->set_rules('first_name', 'First Name', 'trim|required|min_length[5]|max_length[12]');
-        $this->form_validation->set_rules('last_name', 'Last Name', 'trim|required|min_length[5]|max_length[12]');
+        $formData = $_POST['user'];
+        $validation = new Valitron\Validator($formData);
+        $validation->rule('required', 'first_name')->message('First name is required');
+        $validation->rule('required', 'last_name')->message('Last name is required');
+        $validation->rule('required', 'email_address')->message('Email address is required');
+        $validation->rule('required', 'role_id')->message('Role is required');
+        $validation->rule('required', 'password')->message('Password is required');
+        $validation->rule('required', 'confirm_password')->message('Confirm password is required');
+        $validation->rule('lengthMin', 'password', 6);
+        $validation->rule('email', 'email_address');
+        $validation->rule('equals', 'password', 'confirm_password')->message('Password does not matched');
 
-        if ($this->form_validation->run() == FALSE) {
-            $message['error'] = validation_errors();
-            $this->session->set_userdata($message);
-            redirect('dashboard', 'refresh');
+        if (!preg_match('/^[a-zA-Z][a-zA-Z ]*$/', $formData['first_name'])) {
+            $validation->addInstanceRule('firstName', function () {
+                return false;
+            });
+            $validation->rule('firstName', 'first_name')->message('Alphabetic characters only');
         }
 
-        /*$formData = UsersModel::addUser($_POST);
-        var_dump($formData); die();*/
+        if (!preg_match('/^[a-zA-Z][a-zA-Z ]*$/', $formData['last_name'])) {
+            $validation->addInstanceRule('lastName', function () {
+                return false;
+            });
+            $validation->rule('lastName', 'last_name')->message('Alphabetic characters only');
+        }
+
+        if (!$validation->validate()) {
+            $error['error'] = $validation->errors();
+            $oldValue['oldValue'] = $validation->data();
+            $this->session->set_userdata($error);
+            $this->session->set_userdata($oldValue);
+            redirect('users');
+        }
     }
 }
