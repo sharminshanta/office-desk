e8505e928d203c5ba55f3705d226387067e831dc
sharminex81 <mimetyshanta@gmail.com>
2017-08-07 14:44:54 +0600

    Added timezone and country in utilities  classs

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('usersModel', 'roles');
+$autoload['model'] = array('usersModel', 'roles', 'utilities');
