6c32550df4be6c95d3d6b5f94632850251d42ae3
sharminex81 <mimetyshanta@gmail.com>
2017-08-12 23:35:51 +0600

    Roles and permissions is not functional.

--- application/views/users/roles_permissions.php
+++ application/views/users/roles_permissions.php
@@ -19,14 +19,22 @@ $oldValue = $this->session->userdata('oldValue');
     </div>
     <div class="widget-body">
         <form action="<?php echo base_url()?>users/create" method="post" id="signup_signupForm">
-                <?php
-                    foreach ($permissions as $permission) { ?>
-                        <p>
-                            <label for="exampleInputLastName" class="control-label"><?php echo $permission->description; ?></label><br>
-                            <input type="checkbox" id="subscribeNews" name="permission[id]" value="<?php echo $permission->id; ?>">
-                            <?php echo $permission->name; ?>
-                        </p>
-                    <?php } ?>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label for="exampleInputLastName" class="control-label">Role Name</label><br>
+                        <input type="text" id="roleName" name="permission[role_id]" class="form-control" value="<?php echo $role->name; ?>">
+                    </div>
+                </div>
+            </div>
+            <?php
+            foreach ($permissions as $permission) { ?>
+                <p>
+                    <label for="exampleInputLastName" class="control-label"><?php echo $permission->description; ?></label><br>
+                    <input type="checkbox" id="subscribeNews" name="permission[id]" value="<?php echo $permission->id; ?>">
+                    <?php echo $permission->name; ?>
+                </p>
+            <?php } ?>
             <button type="submit" class="btn btn-success" id="signup_btnCreate">Assign Permission</button>
             <br>
         </form>
