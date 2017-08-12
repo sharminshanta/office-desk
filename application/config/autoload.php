32c80d0ff80697fe68faae9df8c89d23a87b1c3a
sharminex81 <mimetyshanta@gmail.com>
2017-08-12 20:22:18 +0600

    Added a model named Permissions_model

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('usersModel', 'roles', 'utilities');
+$autoload['model'] = array('usersModel', 'roles', 'utilities', 'Permissions_model');
