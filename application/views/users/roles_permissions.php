eb07f9d2a11e102b38089a29133ce2d6178870ae
sharminex81 <mimetyshanta@gmail.com>
2017-08-13 11:52:29 +0600

    Chilly changes

--- application/views/users/roles_permissions.php
+++ application/views/users/roles_permissions.php
@@ -1,7 +1,3 @@
-<?php
-$error = $this->session->userdata('error');
-$oldValue = $this->session->userdata('oldValue');
-?>
 <div class="row">
     <div class="col-md-12">
         <div class="jumbotron">
@@ -13,21 +9,21 @@ $oldValue = $this->session->userdata('oldValue');
 <div class="widget">
     <div class="widget-header">
         <div class="pull-left">
-            <h3 class="panel-title">Roles And Permissions</h3>
+            <h3 class="panel-title">Permission for <?php echo $role->name; ?></h3>
         </div>
         <div class="clearfix"></div>
     </div>
     <div class="widget-body">
         <form action="<?php echo base_url()?>roles_Permissions/assignPermission" method="post" id="signup_signupForm">
             <input type="hidden" class="form-control" name="permission[role_id]" value="<?php echo $role->id; ?>">
-            <div class="row">
+            <!--<div class="row">
                 <div class="col-md-4">
                     <div class="form-group">
                         <label for="exampleInputLastName" class="control-label">Role Name</label><br>
-                        <input type="text" disabled="disabled" class="form-control" value="<?php echo $role->name; ?>">
+                        <input type="text" disabled="disabled" class="form-control" value="<?php /*echo $role->name; */?>">
                     </div>
                 </div>
-            </div>
+            </div>-->
             <?php
             foreach ($permissions as $permission) { ?>
                 <p>
