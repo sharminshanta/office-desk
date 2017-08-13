a66623e1fb05d3fe58c2ee68018e5e0cffdbade8
sharminex81 <mimetyshanta@gmail.com>
2017-08-14 02:07:43 +0600

    Remove the unnecessary code

--- application/controllers/Roles_Permissions.php
+++ application/controllers/Roles_Permissions.php
@@ -18,11 +18,22 @@ class Roles_Permissions extends CI_Controller
     {
         $permissions['role'] = Roles_model::details($this->uri->segment(3));
         $permissions['permissions'] = Permissions_model::getPermissions();
+        $permissions['permission'] = Roles_Permissions_model::getAssignPermissions($permissions['role']->id);
+
+        $permissions['permission_names'] = [];
+
+        foreach($permissions['permission'] as $permission)
+        {
+            $permissions['permission_names'] = Roles_Permissions_model::getAssignPermissionsName($permission);
+            //var_dump($permissions['permission_names']);
+        }
+
         $content['header'] = $this->load->view('common/header', '', true);
         $content['navbar'] = $this->load->view('common/navbar', '', true);
         $content['placeholder'] = $this->load->view('users/roles_permissions', $permissions, true);
         $content['footer'] = $this->load->view('common/footer', '', true);
         $this->load->view('dashboard/dashboard', $content);
+        //var_dump($permissions['permission_names']);
     }
 
     /**
