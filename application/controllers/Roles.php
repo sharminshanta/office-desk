66a7108bd61a0790069e25ac13dde5b1329b5960
sharminex81 <mimetyshanta@gmail.com>
2017-08-15 03:49:18 +0600

    User's permission is forced in users and roles controllers

--- application/controllers/Roles.php
+++ application/controllers/Roles.php
@@ -16,11 +16,20 @@ class Roles extends CI_Controller
      */
     public function lists()
     {
-        $roles['roles'] = Roles_model::getRoles();
-        $content['header'] = $this->load->view('common/header', '', true);
-        $content['navbar'] = $this->load->view('common/navbar', '', true);
-        $content['placeholder'] = $this->load->view('roles/default', $roles, true);
-        $content['footer'] = $this->load->view('common/footer', '', true);
-        $this->load->view('dashboard/dashboard', $content);
+        /**
+         * Check the user's permission
+         */
+        $isPermit = Utilities::is_permitTest('roles_lists');
+
+        if ($isPermit == null) {
+            redirect('users');
+        } else {
+            $roles['roles'] = Roles_model::getRoles();
+            $content['header'] = $this->load->view('common/header', '', true);
+            $content['navbar'] = $this->load->view('common/navbar', '', true);
+            $content['placeholder'] = $this->load->view('roles/default', $roles, true);
+            $content['footer'] = $this->load->view('common/footer', '', true);
+            $this->load->view('dashboard/dashboard', $content);
+        }
     }
 }
--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -47,13 +47,21 @@ class Users extends CI_Controller
      */
     public function home()
     {
-        Utilities::is_permit();
-        $roles['roles'] = Roles_model::getRoles();
-        $data['header'] = $this->load->view('common/header', '', true);
-        $data['navbar'] = $this->load->view('common/navbar', '', true);
-        $data['placeholder'] = $this->load->view('users/add', $roles, true);
-        $data['footer'] = $this->load->view('common/footer', '', true);
-        $this->load->view('dashboard/dashboard', $data);
+        /**
+         * Check the user's permission
+         */
+        $isPermit = Utilities::is_permitTest('users_add');
+
+        if ($isPermit == null) {
+            redirect('users');
+        } else {
+            $roles['roles'] = Roles_model::getRoles();
+            $data['header'] = $this->load->view('common/header', '', true);
+            $data['navbar'] = $this->load->view('common/navbar', '', true);
+            $data['placeholder'] = $this->load->view('users/add', $roles, true);
+            $data['footer'] = $this->load->view('common/footer', '', true);
+            $this->load->view('dashboard/dashboard', $data);
+        }
     }
 
     /**
@@ -62,66 +70,75 @@ class Users extends CI_Controller
      */
     public function create()
     {
-        Utilities::is_permit();
-        //@TODO try to manage it with the best way
         /**
-         * Form validation with valitron that is so easy
+         * Check the user's permission
          */
-        $formData = $_POST['user'];
-        $validation = new Valitron\Validator($formData);
-        $validation->rule('required', 'first_name')->message('First name is required');
-        $validation->rule('required', 'last_name')->message('Last name is required');
-        $validation->rule('required', 'email_address')->message('Email address is required');
-        $validation->rule('required', 'role_id')->message('Role is required');
-        $validation->rule('required', 'password')->message('Password is required');
-        $validation->rule('required', 'confirm_password')->message('Confirm password is required');
-        $validation->rule('lengthMin', 'password', 6);
-        $validation->rule('email', 'email_address')->message('This is invalid email address');
-        $userMail = UsersModel::getEmailAddress($formData['email_address']);
-        $validation->rule('equals', 'password', 'confirm_password')->message('Password does not matched');
+        $isPermit = Utilities::is_permitTest('users_add');
 
-        if (!preg_match('/^[a-zA-Z][a-zA-Z ]*$/', $formData['first_name'])) {
-            $validation->addInstanceRule('firstName', function () {
-                return false;
-            });
-            $validation->rule('firstName', 'first_name')->message('Alphabetic characters only');
-        }
-
-        if (!preg_match('/^[a-zA-Z][a-zA-Z ]*$/', $formData['last_name'])) {
-            $validation->addInstanceRule('lastName', function () {
-                return false;
-            });
-            $validation->rule('lastName', 'last_name')->message('Alphabetic characters only');
-        }
-
-        if ($userMail == true) {
-            $validation->addInstanceRule('uMail', function () {
-                return false;
-            });
-            $validation->rule('uMail', 'email_address')->message('This email has already registered');
-        }
-
-        if (!$validation->validate()) {
-            $error['error'] = $validation->errors();
-            $oldValue['oldValue'] = $validation->data();
-            $this->session->set_userdata($error);
-            $this->session->set_userdata($oldValue);
+        if ($isPermit == null) {
             redirect('users');
-        }
+        } else {
+            //@TODO try to manage it with the best way
+            /**
+             * Form validation with valitron that is so easy
+             */
+            $formData = $_POST['user'];
+            $validation = new Valitron\Validator($formData);
+            $validation->rule('required', 'first_name')->message('First name is required');
+            $validation->rule('required', 'last_name')->message('Last name is required');
+            $validation->rule('required', 'email_address')->message('Email address is required');
+            $validation->rule('required', 'role_id')->message('Role is required');
+            $validation->rule('required', 'password')->message('Password is required');
+            $validation->rule('required', 'confirm_password')->message('Confirm password is required');
+            $validation->rule('lengthMin', 'password', 6);
+            $validation->rule('email', 'email_address')->message('This is invalid email address');
+            $userMail = UsersModel::getEmailAddress($formData['email_address']);
+            $validation->rule('equals', 'password', 'confirm_password')->message('Password does not matched');
 
-        try{
-            $userID = UsersModel::addUser();
-            $success = true;
-        }catch (Exception $exception) {
-            $exception->getMessage('This is an error');
-            $success = false;
-        }
+            if (!preg_match('/^[a-zA-Z][a-zA-Z ]*$/', $formData['first_name'])) {
+                $validation->addInstanceRule('firstName', function () {
+                    return false;
+                });
+                $validation->rule('firstName', 'first_name')->message('Alphabetic characters only');
+            }
 
-        if($success == true) {
-            $message['success'] = 'New user has been created successfully';
-            $this->session->set_userdata($message);
-            $user = UsersModel::userInfo($userID);
-            redirect('users/details/' . $user->uuid . '/overview');
+            if (!preg_match('/^[a-zA-Z][a-zA-Z ]*$/', $formData['last_name'])) {
+                $validation->addInstanceRule('lastName', function () {
+                    return false;
+                });
+                $validation->rule('lastName', 'last_name')->message('Alphabetic characters only');
+            }
+
+            if ($userMail == true) {
+                $validation->addInstanceRule('uMail', function () {
+                    return false;
+                });
+                $validation->rule('uMail', 'email_address')->message('This email has already registered');
+            }
+
+            if (!$validation->validate()) {
+                $error['error'] = $validation->errors();
+                $oldValue['oldValue'] = $validation->data();
+                $this->session->set_userdata($error);
+                $this->session->set_userdata($oldValue);
+                redirect('users');
+            }
+
+            try {
+                $userID = UsersModel::addUser();
+                $success = true;
+            } catch (Exception $exception) {
+                $exception->getMessage('This is an error');
+                $success = false;
+            }
+
+            if ($success == true) {
+                $message['success'] = 'New user has been created successfully';
+                $this->session->set_userdata($message);
+                $user = UsersModel::userInfo($userID);
+                redirect('users/details/' . $user->uuid . '/overview');
+            }
+
         }
     }
 
@@ -148,13 +165,21 @@ class Users extends CI_Controller
      */
     public function lists()
     {
-        Utilities::is_permit();
-        $users['users'] = UsersModel::getUsers();
-        $content['header'] = $this->load->view('common/header', '', true);
-        $content['navbar'] = $this->load->view('common/navbar', '', true);
-        $content['placeholder'] = $this->load->view('users/list', $users, true);
-        $content['footer'] = $this->load->view('common/footer', '', true);
-        $this->load->view('dashboard/dashboard', $content);
+        /**
+         * Check the user's permission
+         */
+        $isPermit = Utilities::is_permitTest('users_lists');
+
+        if ($isPermit == null) {
+            redirect('users');
+        } else {
+            $users['users'] = UsersModel::getUsers();
+            $content['header'] = $this->load->view('common/header', '', true);
+            $content['navbar'] = $this->load->view('common/navbar', '', true);
+            $content['placeholder'] = $this->load->view('users/list', $users, true);
+            $content['footer'] = $this->load->view('common/footer', '', true);
+            $this->load->view('dashboard/dashboard', $content);
+        }
     }
 
     /**
