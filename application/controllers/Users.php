2812e7710f061ec25ed3c50acc9ad0902148cb6a
sharminex81 <mimetyshanta@gmail.com>
2017-08-15 18:49:00 +0600

    Code is refactored and user's update is functional.

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -9,24 +9,6 @@ class Users extends CI_Controller
     function __construct()
     {
         parent::__construct();
-
-        /*$user = $this->session->userdata('details');
-
-        if($user == null) {
-            $message['error'] = 'Sorry! Access Denied. You don’t have permission to do.';
-            $this->session->set_userdata($message);
-            redirect('login','refresh');
-        }
-
-        $role = Roles::getName($user['user']->role_id);
-
-        if($role->slug == 'general-user') {
-            $message['error'] = 'Sorry! Access Denied. You don’t have permission to do.';
-            $this->session->set_userdata($message);
-            redirect('login','refresh');
-        }*/
-
-
     }
 
     /**
@@ -143,22 +125,6 @@ class Users extends CI_Controller
     }
 
     /**
-     * User's information
-     * When a new user is created , redirect to his detail page
-     */
-    public function details()
-    {
-        $uuid = $this->uri->segment(3);
-        $userDetails['details'] = UsersModel::userDetails($uuid);
-        $userDetails['role'] = Roles_model::getName($userDetails['details']['user']->role_id);
-        $data['header'] = $this->load->view('common/header', '', true);
-        $data['navbar'] = $this->load->view('common/navbar', '', true);
-        $data['placeholder'] = $this->load->view('users/details', $userDetails, true);
-        $data['footer'] = $this->load->view('common/footer', '', true);
-        $this->load->view('dashboard/dashboard', $data);
-    }
-
-    /**
      * This is for fetching all users.Firstly check user's role
      * if that user is permitted to see a user then he/she would add a new user.
      * Get all users
@@ -183,47 +149,23 @@ class Users extends CI_Controller
     }
 
     /**
-     * Just try pagination
+     * User's information
+     * When a new user is created , redirect to his detail page
      */
-    public function testLists()
-    {
-        $config = array();
-        $config["base_url"] = base_url() . "users/testLists";
-        $config["total_rows"] = UsersModel::record_count();
-        $config["per_page"] = 4;
-        $config["uri_segment"] = 3;
-
-        $this->pagination->initialize($config);
-
-        $page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
-        $data["users"] = UsersModel::getUsers1($config["per_page"], $page);
-        $data["links"] = $this->pagination->create_links();
-        $content['header'] = $this->load->view('common/header', '', true);
-        $content['navbar'] = $this->load->view('common/navbar', '', true);
-        $content['placeholder'] = $this->load->view('users/test_pagination', $data, true);
-        $content['footer'] = $this->load->view('common/footer', '', true);
-        $this->load->view('dashboard/dashboard', $content);
-    }
-
-    /**
-     * Users update view part
-     * User's update with timezone, country
-     */
-    public function update()
+    public function details()
     {
         $uuid = $this->uri->segment(3);
         $userDetails['details'] = UsersModel::userDetails($uuid);
-        $userDetails['timezones'] = Utilities::getTimezones();
-        $userDetails['countries'] = Utilities::getCountries();
-        $content['header'] = $this->load->view('common/header', '', true);
-        $content['navbar'] = $this->load->view('common/navbar', '', true);
-        $content['placeholder'] = $this->load->view('users/update', $userDetails, true);
-        $content['footer'] = $this->load->view('common/footer', '', true);
-        $this->load->view('dashboard/dashboard', $content);
+        $userDetails['role'] = Roles_model::getName($userDetails['details']['user']->role_id);
+        $data['header'] = $this->load->view('common/header', '', true);
+        $data['navbar'] = $this->load->view('common/navbar', '', true);
+        $data['placeholder'] = $this->load->view('users/details', $userDetails, true);
+        $data['footer'] = $this->load->view('common/footer', '', true);
+        $this->load->view('dashboard/dashboard', $data);
     }
 
     /**
-     * Users profile update post
+     * User's profile update view page
      */
     public function profile()
     {
@@ -239,75 +181,22 @@ class Users extends CI_Controller
         $userDetails['roles'] = Roles_model::getRoles();
         $content['header'] = $this->load->view('common/header', '', true);
         $content['navbar'] = $this->load->view('common/navbar', '', true);
-        $content['placeholder'] = $this->load->view('users/update', $userDetails, true);
+        $content['placeholder'] = $this->load->view('users/profile', $userDetails, true);
         $content['footer'] = $this->load->view('common/footer', '', true);
         $this->load->view('dashboard/dashboard', $content);
-        /*$formData = $_POST['profile'];
-        $validation = new Valitron\Validator($formData);
-        $validation->rule('required', 'first_name')->message('First name is required');
-        $validation->rule('required', 'last_name')->message('Last name is required');
-        $validation->rule('required', 'title')->message('Title is required');
-        $validation->rule('required', 'gender')->message('Gender is required');
-
-
-        if (!preg_match('/^[a-zA-Z][a-zA-Z ]*$/', $formData['first_name'])) {
-            $validation->addInstanceRule('firstName', function () {
-                return false;
-            });
-            $validation->rule('firstName', 'first_name')->message('Alphabetic characters only');
-        }
-
-        if (!preg_match('/^[a-zA-Z][a-zA-Z ]*$/', $formData['last_name'])) {
-            $validation->addInstanceRule('lastName', function () {
-                return false;
-            });
-            $validation->rule('lastName', 'last_name')->message('Alphabetic characters only');
-        }
-
-
-        if (!$validation->validate()) {
-            $error['error'] = $validation->errors();
-            $oldValue['oldValue'] = $validation->data();
-            $this->session->set_userdata($error);
-            $this->session->set_userdata($oldValue);
-            redirect('users/update/' . $formData['user_uuid']);
-        }
-
-        try{
-            UsersModel::updateProfile($formData['user_id']);
-            $success = true;
-        }catch (Exception $exception) {
-            $exception->getMessage('This is an error');
-            $success = false;
-        }
-
-        if($success == true) {
-            $message['success'] = 'User has been updated successfully';
-            $this->session->set_userdata($message);
-            $user = UsersModel::userInfo($formData['user_id']);
-            redirect('users/details/' . $user->uuid);
-        }*/
-    }
-
-    /**
-     * Users address update post
-     */
-    public function address()
-    {
-        var_dump($_POST); die();
     }
 
     /**
      * User's profile update post
      */
-    public function profileInfo()
+    public function updateProfile()
     {
         $formData = $_POST['profile'];
         $validation = new Valitron\Validator($formData);
         $validation->rule('required', 'first_name')->message('First name is required');
         $validation->rule('required', 'last_name')->message('Last name is required');
-        $validation->rule('required', 'title')->message('Title is required');
         $validation->rule('required', 'gender')->message('Gender is required');
+        $validation->rule('required', 'timezone')->message('Timezone is required');
 
 
         if (!preg_match('/^[a-zA-Z][a-zA-Z ]*$/', $formData['first_name'])) {
@@ -326,40 +215,42 @@ class Users extends CI_Controller
 
         if ($formData['date_of_birth'] == date("Y-m-d") or $formData['date_of_birth'] > date('Y-m-d')) {
             $validation->addInstanceRule('dateOfBirth', function () {
-               return false;
+                return false;
             });
             $validation->rule('dateOfBirth', 'date_of_birth')->message('Invalid birth date');
         }
 
-        //var_dump($_SERVER['HTTP_REFERER']); die();
-
 
         if (!$validation->validate()) {
             $errors['errors'] = $validation->errors();
-            $oldValue['oldValue'] = $validation->data();
+            $oldValue['oldValues'] = $validation->data();
             $this->session->set_userdata($errors);
             $this->session->set_userdata($oldValue);
-            redirect($_SERVER['HTTP_REFERER']);
+            redirect('users/profile/' . $this->uri->segment(3));
         } else {
-            redirect('users/profile/' . $formData['user_uuid']);
-        }
+            try{
+                UsersModel::updateProfile($formData['user_id']);
+                $success = true;
+            }catch (Exception $exception) {
+                $exception->getMessage('This is an error');
+                $success = false;
+            }
 
-        /*try{
-            UsersModel::updateProfile($formData['user_id']);
-            $success = true;
-        }catch (Exception $exception) {
-            $exception->getMessage('This is an error');
-            $success = false;
+            if($success == true) {
+                $message['success'] = 'User has been updated successfully';
+                $this->session->set_userdata($message);
+                $user = UsersModel::userInfo($formData['user_id']);
+                redirect('users/details/' . $user->uuid);
+            }
         }
+    }
 
-        if($success == true) {
-            $message['success'] = 'User has been updated successfully';
-            $this->session->set_userdata($message);
-            $user = UsersModel::userInfo($formData['user_id']);
-            redirect('users/profile/' . $user->uuid);
-        }
-        $updateProfile = UsersModel::updateProfile($_POST['profile']['user_id']);
-        redirect('users/profile/' . $_POST['profile']['user_uuid']);*/
+    /**
+     * Users address update post
+     */
+    public function address()
+    {
+        var_dump($_POST); die();
     }
 
     /**
