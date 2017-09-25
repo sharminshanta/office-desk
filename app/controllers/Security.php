165612ac592d74cdbee8595807f0b96bf3d3bf69
sharminex81 <mimetyshanta@gmail.com>
2017-09-26 03:39:39 +0600

    forgot password view page is ready now.

--- app/controllers/Security.php
+++ app/controllers/Security.php
@@ -26,9 +26,56 @@ class Security extends CI_Controller
      */
     public function forgotpwd()
     {
-        $postData = $_POST['email_address'];
-        var_dump($postData); die();
+        $email = $_POST['email_address'];
+
+        try {
+            $userDetails = UsersModel::getUserByEmail($email);
+
+            if ($userDetails) {
+                $uniqidId = Utilities::v4();
+                $data = [
+                    'password_token' => $uniqidId
+                ];
+
+                $update = UsersModel::updateUserByEmail($userDetails->uuid, $data);
+                if ($update) {
+                    $link = 'http://localhost:8008/security/passwordReset?email='. $email . '&password_token='.$uniqidId;
+                    var_dump($link); die();
+                }
+            }else {
+                $message['error'] = 'Sorry, Email address does not registered';
+                $this->session->set_userdata($message);
+                redirect('security');
+            }
+        } catch (Exception $exception) {
+            $exception->getMessage();
+        }
     }
 
+    /**
+     * Passwordreset view route
+     */
+    public function passwordReset()
+    {
+        $token = $this->input->get('password_token', TRUE);
 
+        try {
+            if(isset($token)) {
+                $user = UsersModel::getUserByPasswordToken($token);
+                if(isset($user) && $user) {
+                    $content['header'] = $this->load->view('emails/header', '', true);
+                    $content['placeholder'] = $this->load->view('emails/reset_pwd', '', true);
+                    $this->load->view('emails/default', $content);
+                } else {
+                    $message['error'] = 'Sorry, Unauthorised access';
+                    $this->session->set_userdata($message);
+                    $content['header'] = $this->load->view('emails/header', '', true);
+                    $content['placeholder'] = $this->load->view('emails/reset_pwd', '', true);
+                    $this->load->view('emails/default', $content);
+                }
+            }
+        } catch (Exception $exception) {
+            $exception->getMessage();
+        }
+    }
 }
