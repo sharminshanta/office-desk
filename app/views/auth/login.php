a8b26fb036bdf89884023e2d3068ed4cddcc8a00
sharminex81 <mimetyshanta@gmail.com>
2017-10-01 04:40:24 +0600

    icon changed

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -7,7 +7,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta charset="utf-8">
     <title>Login - Besofty Software Limited</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/assets/img/besofty.png" rel="icon"/>
+    <link href="/assets/img/favicon.png" rel="icon"/>
     <!--SCRIPTS-->
     <link href="/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
     <link href="/assets/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
