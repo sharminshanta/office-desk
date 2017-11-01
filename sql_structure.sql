3b45e6ef74c06a8793ca8c76e6b3fe93c2356b90
sharminex81 <mimetyshanta@gmail.com>
2017-11-01 13:33:31 +0600

    Database name is fixed

--- sql_structure.sql
+++ sql_structure.sql
@@ -23,7 +23,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `attendance`
+-- Table structure for table `besofty-desk`
 --
 
 CREATE TABLE `attendance` (
